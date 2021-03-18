05a5d919085fdaa1f5da07d1071ee8206c468345
Harold Wong <harold.wong@microsoft.com>
2021-03-18 15:58:43 -0700

    Add more unit tests

--- src/go/helloworld/helloworld_test.go
+++ src/go/helloworld/helloworld_test.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"os/user"
 	"testing"
 )
 
-func TestSayHello(t *testing.T) {
+func TestSayHelloSuccess(t *testing.T) {
 	expected := "Hello George"
 	actual := sayHello("George")
 	if actual != expected {
 		t.Errorf("Test failed. Expected: '%s',  Got: '%s'", expected, actual)
 	}
+}
+
+func TestSayHelloFail(t *testing.T) {
+	expectedWrong := "Hello Mike"
+	actual := sayHello("George")
+	if actual == expectedWrong {
+		t.Errorf("Test failed. Expected: '%s',  Got: '%s'", expectedWrong, actual)
+	}
+}
+
+func TestGetCurrentUser(t *testing.T) {
+	var userObject *user.User
+	var err error
+
+	userObject, err = user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+	expected := userObject.Username
+	actual := getCurrentUser()
+	if actual != expected {
+		t.Errorf("Test failed. Expected: '%s',  Got: '%s'", expected, actual)
+	}
+}
+
+func TestMain(t *testing.T) {
+	// Test will fail if main errors out
+	main()
 }
