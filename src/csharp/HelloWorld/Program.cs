cc2f801a9957e7a2eb9bb28ad7ef3274ec0013e7
Carl Schweitzer <carl@caschw.com>
2021-04-09 09:23:52 -0500

    Add HelloWorld program

---
+++ src/csharp/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+
+                var current = DateTime.Now;
+                Console.Write(current.ToLongTimeString());
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
