b726e813a360cadc3ae9755e44dd6688fa40bf38
Carl Schweitzer <carl@caschw.com>
2021-04-09 15:45:14 -0500

    Add comments to trigger new build

--- src/csharp/HelloWorld/Program.cs
+++ src/csharp/HelloWorld/Program.cs
@@ -9,9 +9,11 @@ namespace HelloWorld
         {
             while (true)
             {
+                // Reset cursor to beginning of line
                 Console.SetCursorPosition(0, Console.CursorTop);
 
                 var current = DateTime.Now;
+                // Write used to stay on same line
                 Console.Write(current.ToLongTimeString());
 
                 Thread.Sleep(1000);
